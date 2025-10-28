b65265f230124d5a15a11ea35ca7dc40542ea92e
UkkashAhmad <135998226+UkkashAhmad@users.noreply.github.com>
2025-10-28 14:54:51 +0500

    Add BaseDialog component with default props

---
+++ BaseDialog.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog'
+
+const props = withDefaults(defineProps<{
+  title?: string
+  class?:string
+}>(), {
+  title: 'Default Title',
+  class: 'max-w-[800px]'
+})
+
+const isVisible = defineModel<boolean>('visible')
+</script>
+
+<template>
+  <Dialog v-model:open="isVisible">
+    <DialogContent class="text-center" :class="props.class">
+      <DialogHeader>
+        <DialogTitle class="text-center">{{ props.title }}</DialogTitle>
+      </DialogHeader>
+      <slot name="content"/>
+      <DialogFooter>
+        <slot name="footer"/>
+      </DialogFooter>
+    </DialogContent>
+  </Dialog>
+</template>
