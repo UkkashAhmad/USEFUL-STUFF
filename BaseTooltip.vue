f707d7c1c8af72cde60bad2a6046d75d520fc873
UkkashAhmad <135998226+UkkashAhmad@users.noreply.github.com>
2025-10-28 14:57:44 +0500

    Add BaseTooltip component with tooltip functionality

---
+++ BaseTooltip.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip'
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  toolTipContent: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+    <div>
+        <Tooltip>
+            <TooltipTrigger as-child>
+                <p>{{ props.title }}</p>
+            </TooltipTrigger>
+            <TooltipContent>
+                <p>{{ props.toolTipContent }}</p>
+            </TooltipContent>
+        </Tooltip>
+    </div>
+</template>
